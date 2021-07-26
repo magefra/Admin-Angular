a2b6c82e64353f227694ceee87494c16784642ec
magefra9 <magefra9@hotmail.com>
2021-07-26 16:01:14 -0500

    Regresando a la página 1

--- src/app/pages/main-page/users/users.component.ts
+++ src/app/pages/main-page/users/users.component.ts
@@ -127,5 +127,10 @@ export class UsersComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+
   }
 }
